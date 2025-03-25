5814b88720df4631baf0a773d23fc11d2434c585
Manko-C <csalamartin2@gmail.com>
2025-03-25 13:39:45 +0100

    Haladás

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Fooldal from "../src/components/fooldal"
 function App() {
-  
+  return(
+    <Fooldal/>
+  )
 }
 
 export default App;
