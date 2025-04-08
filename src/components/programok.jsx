b3f30a9995202aae685c0df91c587349273ed807
ifmiska23 <mihalyabraham23@gmail.com>
2025-04-08 09:49:06 +0200

    ez van kesz

---
+++ src/components/programok.jsx
@@ -0,0 +1,42 @@
+export default function Programok(){
+    return(
+        <div>
+            <h1>Programok</h1>
+            <div>
+                <h2>csatornasétahajózás idegenvezetővel</h2>
+                <ul>
+                    <li> Elhalad a híres Kis hableány szobor mellett </li>
+                   <li>Élvezze a dán királyi család otthonául szolgáló Amalienborg-palota látványát!</li>
+                   <li>Hajózzon el a dán parlament otthona, a Christiansborg-palota mellett.</li>
+                </ul>
+
+                <h2>Tivoli Gardens belépőjegy korlátlan utazással.</h2>
+                <ul>
+                    <li>Érezze az izgalmakat az olyan hullámvasutakkal, mint a Tejút Expressz hullámvasút.</li> 
+                   <li>Tapasztalja meg a vidámságot a régi autókon való lovaglásban vagy a kísértetház felfedezésében.</li> 
+                   <li>Cápákat láthat, miközben végigsétál a 30 méteres korallzátony-medencén az akváriumban.</li> 
+                </ul>
+            </div>
+
+            <div>
+                <h2>Tivoli Gardens belépőjegy</h2>
+                <ul>
+                    <li>Látogasson el a híres Tivoli kertekbe, és hagyja ki a jegyvásárlási sort</li> 
+                    <li>Csodálja meg a park gyönyörű kertjeit és virágait</li> 
+                    <li>Tapasztalja meg a pörgős utazások izgalmát</li> 
+                    <li>Élvezze a finom ételeket a mesebeli kertekben</li> 
+                    <li>Hallgassa meg a Tivoli Ifjúsági Gárda hangszeres játékát!</li> 
+                </ul>
+
+                <h2>Dán sütemények kóstolótúrája:</h2>
+                <ul>
+                    <li>Kényeztesse minden érzékszervét pelyhes Wienerbrøddel, Kringle-vel és Hindbærsnitterrel.</li> 
+                    <li>Vegyen részt egy vezetett túrán a város öt legjobb dán cukrászdájában.</li> 
+                    <li>Fedezze fel a koppenhágai cukrászkultúra szívét és lelkét, egy-egy falat erejéig.</li> 
+                    <li>Beszélgessen az egyes sütemények és édességek történetébe bepillantást engedő idegenvezetőkkel.</li> 
+                    <li>Kóstolja meg Dánia sütőipari örökségét egyedi ízekkel és gazdag textúrákkal.</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
